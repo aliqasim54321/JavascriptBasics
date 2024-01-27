bcd632ae16bacbe198e2e1ba1f6f0ef1642a34f0
aliqasimkhundmiri10@gmail.com <rehaan@Guest>
2024-01-26 23:45:55 -0500

    started learnig JS

---
+++ 2.js
@@ -0,0 +1,15 @@
+"use strict";
+//intro to variables
+
+//declaring a variable
+var myname = 'Ali Qasim';
+
+//using a variable
+
+console.log(myname);
+
+//changing the value
+
+myname = "khundmiri"
+
+console.log(myname);
